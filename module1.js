372616fa78b5d57b4ec7c293b702964714683b19
cattle4808 <farhodovjasur99@gmail.com>
2025-11-12 12:02:52 +0500

    Refactor module1.js to export a function

--- module1.js
+++ module1.js
@@ -1,16 +1,18 @@
-// module1.js
-const d=document;
-let el=d.createElement('div');
-el.textContent='Модуль импортирован!';
-Object.assign(el.style,{
-  position:'fixed',
-  bottom:'10px',
-  right:'10px',
-  padding:'6px 12px',
-  background:'green',
-  color:'white',
-  fontSize:'14px',
-  borderRadius:'6px',
-  zIndex:2147483647
-});
-d.body.appendChild(el);
+// module.js
+export default function() {
+  const d = document;
+  const el = d.createElement('div');
+  el.textContent = 'Модуль импортирован!';
+  Object.assign(el.style, {
+    position: 'fixed',
+    bottom: '10px',
+    right: '10px',
+    padding: '6px 12px',
+    background: 'green',
+    color: 'white',
+    fontSize: '14px',
+    borderRadius: '6px',
+    zIndex: 2147483647
+  });
+  d.body.appendChild(el);
+}
