c91c79a0a5b9a75ddccc31cb975c1eaaaed2015a
cattle4808 <farhodovjasur99@gmail.com>
2025-11-12 11:54:21 +0500

    Add module1.js to display a notification message

---
+++ module1.js
@@ -0,0 +1,16 @@
+// module1.js
+const d=document;
+let el=d.createElement('div');
+el.textContent='Модуль импортирован!';
+Object.assign(el.style,{
+  position:'fixed',
+  bottom:'10px',
+  right:'10px',
+  padding:'6px 12px',
+  background:'green',
+  color:'white',
+  fontSize:'14px',
+  borderRadius:'6px',
+  zIndex:2147483647
+});
+d.body.appendChild(el);
