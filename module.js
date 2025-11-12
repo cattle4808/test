8e90c8742422dc7db25ba7300e2d4b43259b8f3d
cattle4808 <farhodovjasur99@gmail.com>
2025-11-12 11:38:20 +0500

    Create module.js to display an import notification

---
+++ module.js
@@ -0,0 +1,18 @@
+// module.js
+export default function() {
+  const d = document;
+  let el = d.createElement('div');
+  el.textContent = 'Модуль импортирован!';
+  Object.assign(el.style, {
+    position: 'fixed',
+    bottom: '10px',
+    right: '10px',
+    padding: '6px 12px',
+    background: 'green',
+    color: 'white',
+    fontSize: '14px',
+    borderRadius: '6px',
+    zIndex: 2147483647
+  });
+  d.body.appendChild(el);
+}
