33a6de3bfcb0cffd84d10a0a3074edbdaf9312d0
cattle4808 <farhodovjasur99@gmail.com>
2025-11-12 12:06:58 +0500

    Remove module export and add console log

--- module1.js
+++ module1.js
@@ -1,18 +1,1 @@
-// module.js
-export default function() {
-  const d = document;
-  const el = d.createElement('div');
-  el.textContent = 'Модуль импортирован!';
-  Object.assign(el.style, {
-    position: 'fixed',
-    bottom: '10px',
-    right: '10px',
-    padding: '6px 12px',
-    background: 'green',
-    color: 'white',
-    fontSize: '14px',
-    borderRadius: '6px',
-    zIndex: 2147483647
-  });
-  d.body.appendChild(el);
-}
+console.log("Hi")
